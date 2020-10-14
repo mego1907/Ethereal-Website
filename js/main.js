ebee11b3597bf568c469efd9fdd7faf3dc149b31
mego1907 <ahmed.abd.elmageed.saad.elnmer@gmail.com>
2020-10-15 01:07:51 +0200

    Last UpLoad

--- js/main.js
+++ js/main.js
@@ -1,14 +1,23 @@
-// Slider
+// Add Class Active in NavItem
+const NavbarItems = document.querySelectorAll('.navbar-nav .nav-item .nav-link'); // NavLink
 
+for(let i = 0; i < NavbarItems.length; i++) {
+    NavbarItems[i].addEventListener('click', () => {
+        NavbarItems.forEach((e) => e.classList.remove('active'))
+        NavbarItems[i].classList.add('active');
+    })
+}
+
+
+// Slider
 const sliderContainer = document.getElementById('slider-inner'); // Slider-inner
 const sliderItem = document.querySelectorAll('.slider-item'); // Slider-item
-const points = document.querySelectorAll('.points ul li');
+const points = document.querySelectorAll('.points ul li'); // Li 
 
-let sliderWidth = sliderItem[0].clientWidth; 
-let counter = 1;
+let sliderWidth = sliderItem[0].clientWidth; // Slider Width
+let counter = 1; // Counter
 
 console.log(sliderWidth)
-
 
 setInterval(() => {
     points.forEach((e) => e.classList.remove('active'))
@@ -22,7 +31,6 @@ setInterval(() => {
         //counter = 0;
         counter -= counter;
     }
-
 }, 2500)
 
 function sliderPoints(index) {
@@ -36,3 +44,32 @@ for(let i = 0; i < points.length; i++) {
         points[i].classList.add('active')
     })
 }
+
+
+// Counters
+const counters = document.querySelectorAll('.about .about-counters .counter span'); // Counters
+const aboutSec = document.querySelector('.about'); // About Section
+
+let aboutTop = aboutSec.offsetTop - 500; // About Section Top
+
+document.addEventListener('scroll', () => { 
+    if(document.documentElement.scrollTop >= aboutTop) {
+        setInterval(() => {
+            if(counters[0].innerHTML < 55){
+                counters[0].innerHTML++;
+            }
+            if(counters[1].innerHTML < 88){
+                counters[1].innerHTML++;
+            }
+        } ,300)
+    } else{
+        counters[0].innerHTML = 0; 
+        counters[1].innerHTML = 0;
+    }
+})
+
+
+/* // Add Class Active When I Make Scroll
+document.addEventListener('scroll', () => {
+
+}) */
